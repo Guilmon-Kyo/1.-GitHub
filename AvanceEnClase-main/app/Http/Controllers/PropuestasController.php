e876ce956d7d960db4bd98dab317f8fad91cb17f
Guilmon-Kyo <martin.gonzalez.nunez@gmail.com>
2023-06-02 16:37:11 -0400

    Actualizacion-Trabajo
    Se mueve la carpeta avance en clases a 1.-Github para actualizar de forma específica este repositorio

---
+++ AvanceEnClase-main/app/Http/Controllers/PropuestasController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Propuesta;
+
+class EquipoController extends Controller
+{
+    public function propuestas(){
+        $propuestas = Propuesta::all();
+        return view('propuestas.propuestas',compact('propuestas'));
+    }
+
+    public function store(Request $request){
+        //dd($request->entrenador);
+        $propuesta = new Equipo();
+        $propuesta->nombre = $request->nombre;
+        $propuesta->descripcion = $request->descripcion;
+        $propuesta-> save();
+        return redirect()->route('propuestas.propuestas');
+    }
+}
